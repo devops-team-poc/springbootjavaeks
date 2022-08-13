724b4cacfc37630a5ae08fb837c2db45aca19c2f
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-08-13 14:10:10 +0530

    Update MyfirstappApplication.java

--- src/main/java/com/springboot/app/MyfirstappApplication.java
+++ src/main/java/com/springboot/app/MyfirstappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "MyfirstappApplication.java", + "WelcomeController.java")
 public class MyfirstappApplication {
 
 	public static void main(String[] args) {
