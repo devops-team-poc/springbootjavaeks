84a3960d77ba359d12f9f84fca4b7338d5a99dfb
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-08-15 13:58:36 +0530

    Update WelcomeController.java

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
  @GetMapping("/welcome")
 	public String welcome() {
-	 return "Hurrayyyyyyyyyyyyyyyy!!";
+	 return "Hello world!!";
  }
 }
